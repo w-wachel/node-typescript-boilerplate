f39a7a778586f034015975ab3743e8d813deac51
w-wachel <114748471+w-wachel@users.noreply.github.com>
2026-04-13 19:51:17 +0200

    Update server request parameter to underscore

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import http from 'http';
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((_req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!\n');
